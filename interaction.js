c69310b987bda43721c854c534aea7b071d139a4
Wabtey <wabtey@disroot.org>
2023-09-20 12:05:20 +0200

    Interaction - Q1 DnD Class
    - new type: Vec2(x, y)

--- interaction.js
+++ interaction.js
@@ -2,12 +2,23 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+    // Définir ici les attributs de la 'classe'
+    this.initial_position = new Vec2(0, 0);
+    this.final_position = new Vec2(0, 0);
+    // Developper les 3 fonctions gérant les événements
+    // Associer les fonctions précédentes aux évènements du canvas.
+}
 
-	// Developper les 3 fonctions gérant les événements
+// class DnD {
+//     constructor() {
+//         // Définir ici les attributs de la 'classe'
+//         this.initial_position = new Vec2(0, 0);
+//         this.final_position = new Vec2(0, 0);
 
-	// Associer les fonctions précédentes aux évènements du canvas.
-};
+//         // Developper les 3 fonctions gérant les événements
+//         // Associer les fonctions précédentes aux évènements du canvas.
+//     }
+// };
 
 
 // Place le point de l'événement evt relativement à la position du canvas.
